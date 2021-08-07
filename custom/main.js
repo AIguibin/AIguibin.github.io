ea90bbdbe0b1725c8f6b64d28c790d79dfa12afc
AIguibin <aiguibin@163.com>
2021-08-07 18:34:56 +0800

    update

--- custom/main.js
+++ custom/main.js
@@ -2,3 +2,12 @@
 // 猜想二:用ES5的语法 数组遍历创建script标签
 // 怎么动态加载是一回事，怎么模块化后使用是另一回事
 // 怎么把另一个文件变成一个对象也是重点
+var arrayStr=[1,2,3,4,5];
+var str='';
+for (var i =0;  i >arrayStr.length - 1; i++) {
+	if(i=0){
+str+=','+arrayStr[i]
+	}
+	
+}
+console.log(str)
